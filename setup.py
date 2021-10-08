a844dc305768ce13413e66ea43941c010d9ff2f8
André Pedersen <andrped94@gmail.com>
2021-10-08 13:33:04 +0200

    updated python version dependency

--- setup.py
+++ setup.py
@@ -24,7 +24,10 @@ setuptools.setup(
      },
      install_requires=required,
      classifiers=[
-         "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.6",
+         "Programming Language :: Python :: 3.7",
+         "Programming Language :: Python :: 3.8",
+         "Programming Language :: Python :: 3.9",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
