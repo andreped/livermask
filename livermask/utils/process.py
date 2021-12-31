f7fc1787b2f59f7c67c09480b8b305254f2b5bf2
andreped <andrped94@gmail.com>
2021-12-31 17:50:56 +0100

    working solution

--- livermask/utils/process.py
+++ livermask/utils/process.py
@@ -135,7 +135,6 @@ def vessel_segmenter(curr, output, cpu, verbose, multiple_flag, liver_mask, name
     unet, xp = load_vessel_model(name_vessel, cpu)
 
     # read config
-    log.info("current file path:")
     config = Config(yaml.safe_load(open(os.path.dirname(os.path.abspath(__file__)) + "/../configs/base.yml")))
 
     log.info("resize back...")
