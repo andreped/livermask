68ecff8d83233d9ba44df1ad65361b5e171812be
andreped <andrped94@gmail.com>
2021-12-31 17:40:21 +0100

    typo

--- livermask/utils/process.py
+++ livermask/utils/process.py
@@ -136,7 +136,7 @@ def vessel_segmenter(curr, output, cpu, verbose, multiple_flag, liver_mask, name
 
     # read config
     log.info("current file path:")
-    config = Config(yaml.safe_load(open(os.path.dirname(os.path.abspath(__file__)) + "/configs/base.yml")))
+    config = Config(yaml.safe_load(open(os.path.dirname(os.path.abspath(__file__)) + "/../configs/base.yml")))
 
     log.info("resize back...")
     nib_volume = nib.load(curr)
