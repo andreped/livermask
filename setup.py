e41524cad646c40f619426e9bb1a95726d64e16a
andreped <andrped94@gmail.com>
2022-01-28 00:10:13 +0100

    some encoding issue with reader, tried to use 'rb' and move decoding to later

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import os
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open('requirements.txt', 'r', encoding='utf-8') as ff:
-    required = ff.read().splitlines()
+with open('requirements.txt', 'rb') as ff:
+    required = ff.read().decode("utf-16").splitlines()
 
 
 setup(
